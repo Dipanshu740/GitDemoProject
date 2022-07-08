750c3f686ad8b7e0994fe96e9063e5c4a387cfd7
Dipanshu <Dipanshu@DESKTOP-EPEBV3Q>
2022-07-08 13:47:36 +0530

    First commit--------

---
+++ src/defaultpackages/Test1.java
@@ -0,0 +1,18 @@
+package defaultpackages;
+
+public class Test1 
+{
+   public static void main(String[] args)
+   {
+      String hungry="Yes";
+      
+      if(hungry == "Yes")
+      {
+    	  System.out.println("Eat Something");
+      }
+      else
+      {
+    	  System.out.println("Do your work");
+      }
+}
+}
